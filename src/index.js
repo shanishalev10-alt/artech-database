da9db2d1bd16a88a4f6fba82b0f9184b2014c8e7
shani shalev <shanishalev10@gmail.com>
2026-09-23 17:22:09 +0300

    fixed comments

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(soldierRouter);
-app.use(teamRouter);
+app.use('/soldiers', soldierRouter);
+app.use('/teams', teamRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port", port);
--- src/models/team.js
+++ src/models/team.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Soldier = require("./soldier");
 
 const teamSchema = new mongoose.Schema({
   name: {
@@ -11,10 +12,11 @@ const teamSchema = new mongoose.Schema({
   }
 });
 
-teamSchema.virtual("soldiers", {
-  ref: "Soldier",
-  localField: "_id",
-  foreignField: "team" 
+teamSchema.pre("deleteOne", { query: false, document: true }, async function () {
+  const team = this;
+
+  //delete team property for soldiers who were in this team
+  await Soldier.updateMany({ team: team._id }, { $unset: { team: team._id } });
 });
 
 const Team = mongoose.model("Team", teamSchema);
